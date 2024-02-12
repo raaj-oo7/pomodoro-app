e96fb320db77c295e24ea1f2492ea6733bfedddb
raaj-oo7 <raj.makadia22@gmail.com>
2024-02-12 18:29:34 +0530

    change font awesome icon into svg icon

--- src/components/CheckPomodoroButtons.js
+++ src/components/CheckPomodoroButtons.js
@@ -27,9 +27,27 @@ const CheckPomodoroButtons = ({ completedPomodoros, handlePomodoroCheck, timerMo
   return (
     <div className="pomodoro-progress">
       {completedPomodoros.map((completed, index) => {
-        const buttonIcon = completed ? <i className="fas fa-check"></i> :
-          (delayedFocusLost && !isBreakTime && index === completedPomodoros.findIndex(p => !p)) ?
-            <i className="fas fa-times"></i> : null;
+        const buttonIcon = completed ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            className="svg-icon"
+          >
+            <path fill="none" d="M0 0h24v24H0z" />
+            <path d="M9 16.17L5.53 12.7a.996.996 0 1 1 1.41-1.41l2.83 2.83 6.36-6.36a.996.996 0 1 1 1.41 1.41L10.41 16.17a.996.996 0 0 1-1.41 0z" />
+          </svg>
+        ) : (
+          delayedFocusLost && !isBreakTime && index === completedPomodoros.findIndex(p => !p) ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              className="svg-icon"
+            >
+              <path fill="none" d="M0 0h24v24H0z" />
+              <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z" />
+            </svg>
+          ) : null
+        );
 
         return (
           <button
