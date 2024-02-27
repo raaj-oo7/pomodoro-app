139c4c18b69907c3124149ca02ac628ce6035437
raaj-oo7 <raj.makadia22@gmail.com>
2024-02-27 10:35:59 +0530

    create svg icons file

---
+++ src/assets/svg_icons/SvgIcon.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const CheckMarkIcon = () => (
+  <svg fill="#ffffff" viewBox="0 0 78.369 78.369" stroke="#ffffff">
+    <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+    <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+    <g id="SVGRepo_iconCarrier">
+      <path d="M78.049,19.015L29.458,67.606c-0.428,0.428-1.121,0.428-1.548,0L0.32,40.015c-0.427-0.426-0.427-1.119,0-1.547l6.704-6.704 c0.428-0.427,1.121-0.427,1.548,0l20.113,20.112l41.113-41.113c0.429-0.427,1.12-0.427,1.548,0l6.703,6.704 C78.477,17.894,78.477,18.586,78.049,19.015z"></path>
+    </g>
+  </svg>
+);
+
+export const CrossMarkIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <path d="M13.414 12l5.293-5.293-1.414-1.414-5.293 5.293-5.293-5.293-1.414 1.414 5.293 5.293-5.293 5.293 1.414 1.414 5.293-5.293 5.293 5.293 1.414-1.414-5.293-5.293z" />
+  </svg>
+);
